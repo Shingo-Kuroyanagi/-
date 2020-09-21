ad6baaff14fea0046ee145014ae9287d7571d349
EC2 Default User <ec2-user@ip-172-31-12-245.ec2.internal>
2020-09-21 15:31:21 +0000

    [Update]投稿機能

--- Bookers/app/controllers/books_controller.rb
+++ Bookers/app/controllers/books_controller.rb
@@ -1,13 +1,5 @@
 class BooksController < ApplicationController
   
-
-  def create
-      @book = Book.new(book_params)
-      @books.user_id = current_user.id
-      @book.save  
-      redirect_to book_path(@book.id)
-  end
-  
   def index
     @books = Book.all
     @book = Book.new
@@ -16,27 +8,32 @@ class BooksController < ApplicationController
 
   def show  
     @book = Book.find(params[:id])
-    @user = @book.user
-  end
+    @books = Book.all
+  end  
   
+  def create
+      book = Book.new(book_params)
+      book.save  
+      redirect_to book_path(book.id), notice:"You have created book succesfully"
+  end
   def edit
-     @user = User.find(params[:id])
+    @book = Book.find(params[:id])
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to user_path(@user)
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to user_path(current_user)
   end
   
   def destory
-    @book = Book.find(params[:id])
-    @book.destroy
-    redirect_to book_path
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
 private
   def book_params
-    params.require(:book).permit(:title, :image, :body)
+    params.require(:book).permit(:title, :body)
   end
 end
--- Bookers/app/controllers/users_controller.rb
+++ Bookers/app/controllers/users_controller.rb
@@ -16,19 +16,19 @@ class UsersController < ApplicationController
   end 
   
   def show
-    @book = Book.new
     @user = User.find(params[:id])
-    @books=Book.all
     @user = current_user
+    @book=Book.new
+    @books=Book.all
   end
   
-  def upsate
+  def update
    @user = User.find(params[:id])
    @user.update(user_params)
-   redirect_to user_path(@user.id)
+   redirect_to user_path(@user)
   end
   
   def user_params
-    params.require(:user).permit(:name,:introduction,:profilw_image_id)
+    params.require(:user).permit(:name,:introduction,:profile_image_id)
   end
 end
